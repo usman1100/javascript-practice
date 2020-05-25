79c6925a00818e5f5854762377678578c0d6450b
Usman Ahmed <usmanahmed1100@gmail.com>
2020-05-25 18:41:28 +0500

    Created some functions in object.js

--- objects.js
+++ objects.js
@@ -5,16 +5,49 @@ let box =
     width: 50,
     height: 10,
 
+    // Object Function
     area: function(){
         return this.height * this.width;
     },
 
+    // Arrow functions cannot be used with the THIS keyword
+    // They always point to the global object which is the
+    // window object
     setHeight: function(height){
         this.height = height;
+    },
+
+    // But there's another way to write cleaner functions
+    // for an object
+    setWidth(width){
+        this.width = width;
+    },
+
+    toString(){
+        return ("Height: " + this.height + " Width: " + this.width);
     }
 
 }
 
-console.log(box.height);
+// Array of objects
+let boxes = 
+[
+    {heigth:2, width:5, color: "red"},
+    {heigth:1, width:6, color: "blue"}
+]
+
+
+console.log(box.toString())
 box.setHeight(12)
-console.log(box.height);
+box.setWidth(43)
+console.log(box.toString())
+
+
+
+// Math Object
+
+console.log(Math.PI)
+console.log(Math.E)
+console.log(Math.exp(2))
+
+
