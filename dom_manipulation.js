e0d23cc9ed7f6bf7633c9e6215ccae8b212580c5
Usman <usmanahmed1100@gmail.com>
2020-05-26 15:58:34 +0500

    Added a remove project function, modified some css

--- dom_manipulation.js
+++ dom_manipulation.js
@@ -1,18 +1,44 @@
 let list = document.querySelector(".points");
-let button =document.querySelector(".my_btn");
+let addButton = document.querySelector(".my_btn");
+let deleteButton = document.querySelector("#delete_btn");
+let uls = document.getElementsByTagName("li");
 let newItem = "";
-function addNewItem()
-{
-  let newTitle = document.querySelector("#project_title").value
-  let newLink = document.querySelector("#repo_link").value
-  newItem = document.createElement("li");
-  newItem.innerHTML = 
-  `
-  <a href="${newLink}">${newTitle}</a>
-  `;
-  list.appendChild(newItem);
 
+function addNewItem() {
+
+  let newTitle = document.querySelector("#project_title").value;
+  if(!newTitle)
+  {
+    alert("Enter a valid title")
+  }
+  else
+  {
+    let newLink = document.querySelector("#repo_link").value;
+    
+    newItem = document.createElement("li");
+
+    newItem.innerHTML = `
+    <a href="${newLink}">${newTitle}</a>
+    `;
+
+    list.appendChild(newItem);
+  }
 }
 
 
-button.addEventListener("click", addNewItem)
+function deleteItem(){
+  let projectToDelete = document.querySelector("#search_title").value;
+  for(let i=0; i<uls.length; i++)
+  {
+    if(uls[i].innerText == projectToDelete)
+    {
+      alert("found")
+      list.removeChild(uls[i]);
+    }
+
+    
+  }
+}
+
+addButton.addEventListener("click", addNewItem);
+deleteButton.addEventListener("click", deleteItem);
