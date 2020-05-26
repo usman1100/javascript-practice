7bca918cfa4f3c3024cab4ed288885bf847e7c83
Usman <usmanahmed1100@gmail.com>
2020-05-26 21:04:18 +0500

    Made changes to addNewItem() to changes styles

--- dom_manipulation.js
+++ dom_manipulation.js
@@ -4,6 +4,7 @@ let deleteButton = document.querySelector("#delete_btn");
 let uls = document.getElementsByTagName("li");
 let newItem = "";
 
+
 function is_url(str)
 {
   regexp =  /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
@@ -40,10 +41,15 @@ function addNewItem() {
     <a href="${newLink}">${newTitle}</a>
   `;
 
+  if(newTitle.includes("boop"))
+    newItem.querySelector("a").classList.add("redText");
+
   list.appendChild(newItem);
 
-}
+  document.querySelector("#repo_link").value = "";
+  document.querySelector("#project_title").value = "";
 
+}
 
 function deleteItem(){
   let projectToDelete = document.querySelector("#search_title").value;
@@ -67,5 +73,8 @@ function deleteItem(){
   alert(projectToDelete + " not found")
 }
 
+
+
 addButton.addEventListener("click", addNewItem);
 deleteButton.addEventListener("click", deleteItem);
+
