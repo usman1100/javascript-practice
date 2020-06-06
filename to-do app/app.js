1a75104444d02bdec217fc7190d1aeba2921c379
usman <usmanahmed1100@gmail.com>
2020-06-06 15:21:53 +0500

    Added search function

--- to-do app/app.js
+++ to-do app/app.js
@@ -1,32 +1,57 @@
-let doneButtons = document.querySelectorAll("#done")
-let deleteButtons = document.querySelectorAll("#delete")
 let addButton = document.querySelector("#taskButton")
-let par = null
-
-
-
 let container = document.querySelector("#container")
+let search = document.querySelector("#searchInput")
 
-container.addEventListener("click", e=>{
-       if(e.target.id === "delete"){
-              e.target.parentElement.remove();
-       }
-       
-       if(e.target.id === "done"){
-              e.target.parentElement.style.backgroundColor = "green";
-              e.target.style.display = "none"
-       }
+
+
+search.addEventListener("keyup", () => {
+    let searchTerm = search.value;
+    let items = document.querySelectorAll(".message");
+    if (items.length > 0  && !(searchTerm === "")) {
+        items.forEach(item => {
+            if (!item.textContent.trim().includes(searchTerm)) {
+                item.parentElement.style.display = "none"
+            }
+        })
+
+        items.forEach(item => {
+            if (item.textContent.trim().includes(searchTerm)) {
+                item.parentElement.style.display = "block"
+            }
+        })
+    }
+
+    else if(searchTerm === "")
+    {
+        items.forEach(item => {
+            item.parentElement.style.display = "block"
+            
+        })
+    }
+
+})
+
+
+container.addEventListener("click", e => {
+    if (e.target.id === "delete") {
+        e.target.parentElement.remove();
+    }
+
+    if (e.target.id === "done") {
+        e.target.parentElement.style.backgroundColor = "green";
+        e.target.style.display = "none"
+    }
 })
 
 
 
 
 addButton.addEventListener("click", () => {
-       let task = document.querySelector("#taskName").value.trim()
+    let task = document.querySelector("#taskName").value.trim()
 
-       if (task) {
-              let html =
-                     `
+    if (task) {
+        let html =
+            `
                      <div class="todo-item my-3">
                      <i class="fa fa-trash fa-2x icon" id="delete"></i>
                      <i class="fa fa-check-circle fa-2x icon" id="done"></i>
@@ -36,9 +61,8 @@ addButton.addEventListener("click", () => {
                      </div>
 
                      `
-              
-              document.querySelector("#taskName").value = ""
-              document.querySelector("#container").innerHTML += html
-       }
-})
 
+        document.querySelector("#taskName").value = ""
+        document.querySelector("#container").innerHTML += html
+    }
+})
