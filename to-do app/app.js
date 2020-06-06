0885774b8d53d31a8dcff5f63a0bbf77cd3b43e2
usman <usmanahmed1100@gmail.com>
2020-06-06 09:03:06 +0500

    Done/Delete now works with new added tasks

--- to-do app/app.js
+++ to-do app/app.js
@@ -1,24 +1,48 @@
 let doneButtons = document.querySelectorAll("#done")
 let deleteButtons = document.querySelectorAll("#delete")
+let addButton = document.querySelector("#taskButton")
 let par = null
 
 
-
-doneButtons.forEach(button => {
- button.addEventListener("click",()=>{
-        par = button.parentElement
-        par.style.backgroundColor = "green";
-        button.style.display = "none";
- })
+const addDoneEvents =()=> doneButtons.forEach(button => {
+       button.addEventListener("click", () => {
+              par = button.parentElement
+              par.style.backgroundColor = "green";
+              button.style.display = "none";
+       })
 });
 
 
-deleteButtons.forEach(button => {
-    button.addEventListener("click",()=>{
-           par = button.parentElement
-           par.style.display = "none";
-    })
-   });
+const addDeleteEvents =()=> deleteButtons.forEach(button => {
+       button.addEventListener("click", () => {
+              par = button.parentElement
+              par.style.display = "none";
+       })
+});
 
 
+addButton.addEventListener("click", () => {
+       let task = document.querySelector("#taskName").value.trim()
+
+       if (task) {
+              let html =
+                     `
+                     <div class="todo-item my-3">
+                     <i class="fa fa-trash fa-2x icon" id="delete"></i>
+                     <i class="fa fa-check-circle fa-2x icon" id="done"></i>
+                     <div class="message">
+                            ${task}
+                     </div>
+                     </div>
+
+                     `
+              
+              document.querySelector("#taskName").value = ""
+              document.querySelector("#container").innerHTML += html
+              doneButtons = document.querySelectorAll("#done")
+              deleteButtons = document.querySelectorAll("#delete")
+              addDoneEvents()
+              addDeleteEvents()
+       }
+})
 
