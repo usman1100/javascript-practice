9a99732aa7d66272031f7a953d5e1351f5cd65fa
usman <usmanahmed1100@gmail.com>
2020-06-10 15:35:23 +0500

    Finalized Gradient Generator

--- gradient generator/app.js
+++ gradient generator/app.js
@@ -1,11 +1,13 @@
 let c1 = document.querySelector("#c1");
 let c2 = document.querySelector("#c2");
 let body = document.querySelector("body");
-
+let css = document.querySelector("#css");
 
 
 const generateGradient = () =>{
-    body.style.background = `linear-gradient(to right, ${c1.value}, ${c2.value})`
+    body.style.background = `linear-gradient(to left, ${c1.value}, ${c2.value})`;
+    css.textContent = body.style.background;
+
 }
 
 c1.addEventListener("input", ()=>{
