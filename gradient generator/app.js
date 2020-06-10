85de62bf04f7adfd2d6434efd7b524611e2698ae
usman <usmanahmed1100@gmail.com>
2020-06-10 14:48:10 +0500

    Made changes, reverted them

--- gradient generator/app.js
+++ gradient generator/app.js
@@ -2,6 +2,8 @@ let c1 = document.querySelector("#c1");
 let c2 = document.querySelector("#c2");
 let body = document.querySelector("body");
 
+
+
 const generateGradient = () =>{
     body.style.background = `linear-gradient(to right, ${c1.value}, ${c2.value})`
 }
