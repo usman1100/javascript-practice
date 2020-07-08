1fbcf2f4b01b9bce64544f908dba92d95b86dc10
usman <usmanahmed1100@gmail.com>
2020-07-08 07:37:08 +0500

    Deleted node modules

--- Asyncrounous/callback_hell.js
+++ Asyncrounous/callback_hell.js
@@ -1,6 +1,6 @@
 const request = new XMLHttpRequest()
 
-request.addEventListener("readystatechange", ()=>{console.log(request.readyState)})
+request.addEventListener("readystatechange", ()=>{console.log(request.statusText)})
 
 
 request.open("GET","./data.json")
