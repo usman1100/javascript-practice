0dff33846b1a91823dd7e25e10ac3b954597ae0f
usman <usmanahmed1100@gmail.com>
2020-07-08 08:29:44 +0500

    Callback hell done

--- Asyncrounous/callback_hell.js
+++ Asyncrounous/callback_hell.js
@@ -1,7 +1,32 @@
-const request = new XMLHttpRequest()
+const makeRequest = (callback) => {
 
-request.addEventListener("readystatechange", ()=>{console.log(request.statusText)})
+    const request = new XMLHttpRequest()
+    request.addEventListener("readystatechange", () => {
+        if (request.readyState === 4 && request.status === 200)
+            callback(undefined, request.responseText);
+
+        else if (request.readyState === 4)
+            callback("404 Error", undefined)
+    })
+
+    request.open("GET", "./data.json")
+    request.send()
+}
 
 
-request.open("GET","./data.json")
-request.send()
+makeRequest((err, data) => {
+
+    if(err) console.log(err)
+    console.log(data);
+    makeRequest((err, data) => {
+
+        if(err) console.log(err)
+        console.log(data);
+        makeRequest((err, data) => {
+            
+            if(err) console.log(err)
+            console.log(data);
+        })
+    })
+
+})
